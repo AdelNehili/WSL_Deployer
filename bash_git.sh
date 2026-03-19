c3b306fe8969c3569cf438da52d42d6af3f15eac
AdelNehili <a.nehili@hotmail.com>
2026-03-19 04:27:28 +0100

    clean files

--- bash_git.sh
+++
@@ -1,7 +0,0 @@
-echo "# WSL_Deployer" >> README.md
-git init
-git add *
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/AdelNehili/WSL_Deployer.git
-git push -u origin main
